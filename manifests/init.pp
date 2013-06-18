8ce763899b4d6bfde97e838df0c67e163bb0ecab
Michael Chapman <michchap@cisco.com>
2013-06-18 12:14:38 +1000

    Pull in head from jsosic's mercurial repo

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,149 +1,198 @@
-# = Class: cobbler
+# Class: cobbler
 #
-# This class installs and configures Cobbler, the PXE boot install engine.  Currently 
-# it only supports Ubuntu deployments, but can be extended to support Debian, and RedHat
-# style systems.
-# 
-# == Local Parameters:
-# - $node_subnet		The subnet for the PXE server, only one subnet is supported
-# - $node_netmask 		The netmask for the subnet
-# - $node_gateway		The default gateway for the subnet
-# - $node_dns			The DNS server IP address (currently cobbler doesn't manage DNS)
-# - $domain_name		The domain name relative to the new nodes being deployed
-# - $ip					The IP address that the cobbler server will use as the "next hop"
-# - $no_default_route	Do not install a default route when bringing up the network
-# - $dhcp_ip_low		The low range of default DHCP address
-# - $dhcp_ip_high		The high range of default DHCP addresses
-# - $dns_service		Manage DNS? defaults to no. options: dnsmasq, isc
-# - $dhcp_service		Manage DHCP? defaults to no. options: dnsmasq, isc
-# - $ntp_server			NTP server if the default (ntp.ubuntu.com) isn't reachable
-# - $proxy = ''			If a proxy is required to get to the internet for updates, define it
-# - $password_crypted = "x"  - The default 'localadmin' user password, MD5 encrypted. 
-# - $ucsm_port                 The port at which the ucsm accepts requests(for ucs bseries only). Default value is 443(https)
-# 
-# == Requires:
-# 
-# Nothing.
-# 
-# == Sample Usage:
-# 
-# Note: the password below is "ubuntu"
-# 
-#	node /cobbler\.example\.com/ {
-#		class { cobbler:
-#			node_subnet => "192.168.1.0",
-#			node_netmask => "255.255.255.0",
-#			node_gateway => "192.168.1.1",
-#			node_dns => "192.168.1.1",
-#			ip => '192.168.1.254',
-#			domain_name => "example.com",
-#			proxy => "http://192.168.1.1:3142/",
-#			password_crypted => '$6$UfgWxrIv$k4KfzAEMqMg.fppmSOTd0usI4j6gfjs0962.JXsoJRWa5wMz8yQk4SfInn4.WZ3L/MCt5u.62tHDGB36EhiKF1',
-#	}
+# This class manages Cobbler
+# https://fedorahosted.org/cobbler/
 #
-class cobbler(
-	$node_subnet,
-	$node_netmask,
-	$node_gateway = false,
-	$node_dns,
-	$domain_name,
-	$ip,
-	$no_default_route = undef,
-	$dhcp_ip_low = undef,
-	$dhcp_ip_high = undef,
-	$proxy = '',
-	$ucs_org = '',
-	$dns_service = undef,
-	$dhcp_service = undef,
-	$ntp_server = undef,
-	$password_crypted = "x",
-        $ucsm_port)
-{
+# Parameters:
+#
+#   - $service_name [type: string]
+#     Name of the cobbler service, defaults to 'cobblerd'.
+#
+#   - $package_name [type: string]
+#     Name of the installation package, defaults to 'cobbler'
+#
+#   - $package_ensure [type: string]
+#     Defaults to 'present', buy any version can be set
+#
+#   - $distro_path [type: string]
+#     Defines the location on disk where distro files will be
+#     stored. Contents of the ISO images will be copied over
+#     in these directories, and also kickstart files will be
+#     stored. Defaults to '/distro'
+#
+#   - $manage_dhcp [type: bool]
+#     Wether or not to manage ISC DHCP.
+#
+#   - $dhcp_dynamic_range [type: string]
+#     Range for DHCP server
+#
+#   - $manage_dns [type: string]
+#     Wether or not to manage DNS
+#
+#   - $dns_option [type: string]
+#     Which DNS deamon to manage - Bind or dnsmasq. If dnsmasq,
+#     then dnsmasq has to be used for DHCP too.
+#
+#   - $manage_tftpd [type: bool]
+#     Wether or not to manage TFTP daemon.
+#
+#   - $tftpd_option [type:string]
+#     Which TFTP daemon to use.
+#
+#   - $server_ip [type: string]
+#     IP address of a server.
+#
+#   - $next_server_ip [type: string]
+#     Next Server in cobbler config.
+#
+#   - $nameserversa [type: array]
+#     Nameservers for kickstart files to put in resolv.conf upon
+#     installation.
+#
+#   - $dhcp_interfaces [type: array]
+#     Interface for DHCP to listen on.
+#
+#   - $defaultrootpw [type: string]
+#     Hash of root password for kickstart files.
+#
+#   - $apache_service [type: string]
+#     Name of the apache service.
+#
+#   - $allow_access [type: string]
+#     For what IP addresses/hosts will access to cobbler_api be granted.
+#     Default is for server_ip, ::ipaddress and localhost
+#
+#   - $purge_distro  [type: bool]
+#   - $purge_repo    [type: bool]
+#   - $purge_profile [type: bool]
+#   - $purge_system  [type: bool]
+#     Decides wether or not to purge (remove) from cobbler distro,
+#     repo, profiles and systems which are not managed by puppet.
+#     Default is true.
+#
+# Actions:
+#   - Install Apache
+#   - Manage Apache service
+#
+# Requires:
+#   - puppetlabs/apache class
+#     (http://forge.puppetlabs.com/puppetlabs/apache)
+#
+# Sample Usage:
+#
+class cobbler (
+  $service_name       = $cobbler::params::service_name,
+  $package_name       = $cobbler::params::package_name,
+  $package_ensure     = $cobbler::params::package_ensure,
+  $distro_path        = $cobbler::params::distro_path,
+  $manage_dhcp        = $cobbler::params::manage_dhcp,
+  $dhcp_dynamic_range = $cobbler::params::dhcp_dynamic_range,
+  $manage_dns         = $cobbler::params::manage_dns,
+  $dns_option         = $cobbler::params::dns_option,
+  $manage_tftpd       = $cobbler::params::manage_tftpd,
+  $tftpd_option       = $cobbler::params::tftpd_option,
+  $server_ip          = $cobbler::params::server_ip,
+  $next_server_ip     = $cobbler::params::next_server_ip,
+  $nameservers        = $cobbler::params::nameservers,
+  $dhcp_interfaces    = $cobbler::params::dhcp_interfaces,
+  $defaultrootpw      = $cobbler::params::defaultrootpw,
+  $apache_service     = $cobbler::params::apache_service,
+  $allow_access       = $cobbler::params::allow_access,
+  $purge_distro       = $cobbler::params::purge_distro,
+  $purge_repo         = $cobbler::params::purge_repo,
+  $purge_profile      = $cobbler::params::purge_profile,
+  $purge_system       = $cobbler::params::purge_system,
+) inherits cobbler::params {
 
-	package { cobbler:
-		ensure => present
-	}
+  # require apache modules
+  require apache::mod::wsgi
+  require apache::mod::proxy
+  require apache::mod::proxy_http
 
-	$dns_package = $dns_service ? {
-		'dnsmasq' => 'dnsmasq',
-		'isc' => 'bind9',
-		default => undef,
-	}
+  # install section
+  package { 'tftp-server': ensure => present, }
+  package { 'syslinux':    ensure => present, }
+  package { $package_name :
+    ensure  => $package_ensure,
+    require => [ Package['syslinux'], Package['tftp-server'], ],
+  }
 
-	$dhcp_package = $dhcp_service ? {
-		'dnsmasq' => 'dnsmasq',
-		'isc' => 'dhcp3-server',
-		default => undef,
-	}
+  service { $service_name :
+    ensure  => running,
+    enable  => true,
+    require => Package[$package_name],
+  }
 
-	if ($dns_package) {
-		package { "$dns_package":
-			ensure => present
-		}
-	}
-  
-	if ($dhcp_package) {
-		if ($dhcp_package != $dns_package) {
-			package { $dhcp_package:
-				ensure => present
-			}
-		}
-	}
+  # file defaults
+  File {
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+  }
+  file { '/etc/httpd/conf.d/proxy_cobbler.conf':
+    content => template('cobbler/proxy_cobbler.conf.erb'),
+    notify  => Service[$apache_service],
+  }
+  file { $distro_path :
+    ensure => directory,
+    mode   => '0755',
+  }
+  file { "${distro_path}/kickstarts" :
+    ensure => directory,
+    mode   => '0755',
+  }
+  file { '/etc/cobbler/settings':
+    content => template('cobbler/settings.erb'),
+    require => Package[$package_name],
+    notify  => Service[$service_name],
+  }
+  file { '/etc/cobbler/modules.conf':
+    content => template('cobbler/modules.conf.erb'),
+    require => Package[$package_name],
+    notify  => Service[$service_name],
+  }
+  file { '/etc/httpd/conf.d/distros.conf': content => template('cobbler/distros.conf.erb'), }
+  file { '/etc/httpd/conf.d/cobbler.conf': content => template('cobbler/cobbler.conf.erb'), }
 
-	package { "tftpd-hpa":
-		ensure => present
-	}
+  # cobbler sync command
+  exec { 'cobblersync':
+    command     => '/usr/bin/cobbler sync',
+    refreshonly => true,
+  }
 
-	file { "/etc/cobbler":
-		ensure => directory
-	}
+  # purge resources
+  if $purge_distro == true {
+    resources { 'cobblerdistro':  purge => true, }
+  }
+  if $purge_repo == true {
+    resources { 'cobblerrepo':    purge => true, }
+  }
+  if $purge_profile == true {
+    resources { 'cobblerprofile': purge => true, }
+  }
+  if $purge_system == true {
+    resources { 'cobblersystem':  purge => true, }
+  }
 
-	file { "/etc/cobbler/settings":
-		content => template('cobbler/settings.erb'),
-		require => File["/etc/cobbler"],
-		notify => Exec["restart-cobbler"],
-	}
-
-	file { "/etc/cobbler/modules.conf":
-		content => template('cobbler/modules.conf.erb'),
-		require => File["/etc/cobbler"],
-		notify => Exec["restart-cobbler"],
-	}
-
-	file { "/etc/cobbler/dhcp.template":
-		content => template('cobbler/dhcp.template.erb'),
-		require => File["/etc/cobbler"],
-		notify => Exec["restart-cobbler"],
-	}
-	
-	file { "/etc/cobbler/dnsmasq.template":
-		content => template('cobbler/dnsmasq.template.erb'),
-		require => File["/etc/cobbler"],
-		notify => Exec["restart-cobbler"],
-	}
-	
-	file { "/etc/cobbler/power":
-		ensure => directory,
-		require => File["/etc/cobbler"],
-	}
-
-	file { "/etc/cobbler/power/power_ucs.template":
-		content => template('cobbler/power_ucs_domain.erb'),
-		require => File["/etc/cobbler/power"],
-	}
-
-	exec { "restart-cobbler":
-		command => "/usr/sbin/service cobbler restart",
-		refreshonly => true,
-		require => Package[cobbler],
-	}
-
-	exec { "cobbler-sync":
-		command => "/usr/bin/cobbler sync",
-		provider => shell,
-		refreshonly => true,
-		before => Exec[restart-cobbler],
-		require => Package[cobbler],
-	}
-
+  # include ISC DHCP only if we choose manage_dhcp
+  if $manage_dhcp == '1' {
+    package { 'dhcp':
+      ensure => present,
+    }
+    service { 'dhcpd':
+      ensure  => running,
+      require => Package['dhcp'],
+    }
+    file { '/etc/cobbler/dhcp.template':
+      ensure  => present,
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      content => template('cobbler/dhcp.template.erb'),
+      require => Package[$package_name],
+      notify  => Exec['cobblersync'],
+    }
+  }
 }
+# vi:nowrap:
